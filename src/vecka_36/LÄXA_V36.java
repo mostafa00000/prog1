b8f1e512084d0f65158989d19444cf9b34e7946c
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-09-19 12:54:26 +0200

    jlkjlmklj

--- src/vecka_36/LÄXA_V36.java
+++ src/vecka_36/LÄXA_V36.java
@@ -1,5 +1,19 @@
 package vecka_36;
 
+import java.util.Scanner;
+
 public class LÄXA_V36 {
 
+	  public static void main(String[] args) {
+		  
+		  Scanner input = new Scanner(System.in);
+		  
+		  	
+	
+		  
+		  
+		  
+	}
+	
+	
 }
---
+++ src/vecka_36/vilkor.java
@@ -0,0 +1,44 @@
+package vecka_36;
+import java.util.Scanner;
+public class vilkor {
+
+	public static void main(String[] arg) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("skriv in 10 heltal");
+		
+		int t1 = input.nextInt();
+		int min = t1;
+		int max = t1;
+		
+		int t2 = input.nextInt();
+		
+		if(t2 > max ) {
+			t2=max; 
+		}		
+		if t2 < min) {
+			t2=m9n;
+		}
+		int t3 = input.nextInt();
+		int t4 = input.nextInt();
+		int t5 = input.nextInt();
+		int t6 = input.nextInt();
+		int t7 = input.nextInt();
+		int t8 = input.nextInt();
+		int t9 = input.nextInt();
+		int t10 = input.nextInt();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// crt+c ctrl+v
+}
