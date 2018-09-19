d46fda40b6fcec2130a561874567864463178d10
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-09-19 13:08:25 +0200

    done?

--- src/vecka_36/vilkor.java
+++ src/vecka_36/vilkor.java
@@ -15,21 +15,96 @@ public class vilkor {
 		
 		int t2 = input.nextInt();
 		
-		if(t2 > max ) {
-			t2=max; 
+		if (t2 > max ) {
+			max = t2; 
 		}		
-		if t2 < min) {
-			t2=m9n;
+		if (t2 < min) {
+			min = t2;
 		}
 		int t3 = input.nextInt();
+		
+		if (t3 > max ) {
+			max = t3; 
+		}		
+		if (t3 < min) {
+			min = t3;
+		}
+		
 		int t4 = input.nextInt();
+		
+		
+		if (t4 > max ) {
+			max = t4; 
+		}		
+		if (t4 < min) {
+			min = t4;
+		}
+		
 		int t5 = input.nextInt();
+		
+		
+		if (t5 > max ) {
+			max = t5; 
+		}		
+		if (t5 < min) {
+			min = t5;
+		}
+		
 		int t6 = input.nextInt();
+		
+		
+		if (t6 > max ) {
+			max = t6; 
+		}		
+		if (t6 < min) {
+			min = t6;
+		}
+		
 		int t7 = input.nextInt();
+		
+		if (t7 > max ) {
+			max = t7; 
+		}		
+		if (t7 < min) {
+			min = t7;
+		}
+		
 		int t8 = input.nextInt();
+		
+		if (t8 > max ) {
+			max = t8; 
+		}		
+		if (t8 < min) {
+			min = t8;
+		}
+		
 		int t9 = input.nextInt();
+		
+		if (t9 > max ) {
+			max = t9; 
+		}		
+		if (t9 < min) {
+			min = t9;
+		}
+		
 		int t10 = input.nextInt();
+		
+		if (t10 > max ) {
+			max = t10; 
+		}		
+		if (t10 < min) {
+			min = t10;
+		}
+	
+          double summa = t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9; 
+	
+	System.out.println("maxtal: " + max);
+	System.out.println("mintal: " + min);
+    System.out.println("medeltal: " + summa / 10);
+	
 	}
+	
+	
 	
 	
 	
