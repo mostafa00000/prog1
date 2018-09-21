af84d6507a022648f965f4696c86be861faae06d
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-09-21 14:32:23 +0200

    kattis r2

---
+++ src/lektion_projekt/kattis.java
@@ -0,0 +1,20 @@
+package lektion_projekt;
+
+import java.util.Scanner;
+
+public class kattis {
+
+	public static void main(String[] args) {
+		  
+		
+		Scanner input = new Scanner(System.in);
+		
+	 int r2;
+	 int s = input.nextInt();
+	 int r1	= input.nextInt();
+  
+	System.out.println(r1*2-s );
+	 
+	}
+	 
+}
---
+++ src/lektion_projekt/loops.java
@@ -0,0 +1,15 @@
+package lektion_projekt;
+
+public class loops {
+	public static void main(String[] args) {
+         int i = >25;
+		for (int i = 25; i < 50; i++)	{
+			int summa =  50;
+
+		}
+		System.out.println("summa :" + summa );
+      
+	
+	}
+	
+}
