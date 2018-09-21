5a6af043f36b85be0aa4b60d4cbc1134b45b595e
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-09-21 15:10:02 +0200

    time loop

---
+++ src/lektion_projekt/time_loop.java
@@ -0,0 +1,24 @@
+package lektion_projekt;
+
+import java.util.Scanner;
+
+public class time_loop {
+
+	public static void main(String[] args) {
+		
+		 Scanner input = new Scanner(System.in);
+		 
+		 int x =1;  
+		 int I = input.nextInt();
+		 
+		 while (x <= I) {
+		
+		  System.out.println(x + " Abracadabra"); 
+		  x++;
+		 }
+		 
+		 
+		
+	}
+	
+}
