4cfabb3613095d0450248ba9b158db339a5c0166
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-09-29 10:22:09 +0200

    denhär var fan svår :(

--- src/lektion_projekt/Spavanac.java
+++ src/lektion_projekt/Spavanac.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 public class Spavanac {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+    	Scanner input = new Scanner(System.in);
+                
+            int hour = input.nextInt();
+            int min = input.nextInt();
+            if(min<45){
+                if(hour<1){
+                    System.out.println(23+" "+(min+15));
+                }else{
+                    System.out.println((hour-1)+" "+(min+15));
+                }
+            }else{
+                System.out.println(hour+" "+(min-45));
+            }
+        }
+    }
+
 		
-		Scanner input = new Scanner(System.in);
 		
 		
 		
-		
-	}
 	
 	
-}
+	
+
