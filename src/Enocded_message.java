5c397f013370b986ed58ca0c39276d79cb104677
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2019-02-01 14:55:19 +0100

    Update src/Enocded_message.java

---
+++ src/Enocded_message.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Encodedmessage {
+    public static void main(String[] args)
+    
+    
+    { Scanner input = new Scanner(System.in);
+    
+    
+    int testcase = input.nextInt(); int iterations  = 0;
+    
+    
+    boolean skipped = false; while (iterations < testcase)
+    {
+        
+        String encoded = input.next(); String decoded = "";
