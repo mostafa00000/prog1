7bd9d7ec196090d50b1fddee9a86602d75657a4a
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2019-02-01 15:06:19 +0100

    Update src/Enocded_message.java

--- src/Enocded_message.java
+++ src/Enocded_message.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
+    public class Enocded_message {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-public class Encodedmessage {
-    public static void main(String[] args)
-    
-    
-    { Scanner input = new Scanner(System.in);
-    
-    
-    int testcase = input.nextInt(); int iterations  = 0;
-    
-    
-    boolean skipped = false; while (iterations < testcase)
-    {
+    int TESTCASES = scan.nextInt();
+    scan.nextLine();
+
+    for (int z = 0; z < TESTCASES; z++)
+        {
+ String str = scan.nextLine();
+int len = (int)Math.sqrt(str.length());
         
-        String encoded = input.next(); String decoded = "";
+ for (int j = 0; j < len; j++)
+ for (int i = len - 1; i < str.length(); i += len)
+  System.out.print(str.charAt(i - j));
+        
+        System.out.println();
+        }
+
+    scan.close();
+
+        }
+
+}
