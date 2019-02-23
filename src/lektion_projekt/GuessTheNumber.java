473916f1866face374c9f18003a66932eb78c688
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2019-02-23 14:56:13 +0100

    Create GuessTheNumber.java

---
+++ src/lektion_projekt/GuessTheNumber.java
@@ -0,0 +1,21 @@
+package lektion_projekt;
+
+import java.util.Scanner;
+import java.util.Random; 
+public class GuessTheNumber {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner YEET = new Scanner(System.in);
+		
+		
+		 Random rand = new Random(); 
+		 int randomTal = rand.nextInt(100);
+		 
+	
+	
+	}
+	
+	
+}
