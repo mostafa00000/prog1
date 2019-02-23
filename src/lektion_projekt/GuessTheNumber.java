c9ba51dffccdcf1766081ec04f58a93894aacf3f
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2019-02-23 15:29:20 +0100

    Update GuessTheNumber.java
    klart!
    
    :)

--- src/lektion_projekt/GuessTheNumber.java
+++ src/lektion_projekt/GuessTheNumber.java
@@ -9,12 +9,38 @@ public class GuessTheNumber {
 		
 		Scanner YEET = new Scanner(System.in);
 		
-		
+		// genererar ett slumpmessigt  tal
 		 Random rand = new Random(); 
-		 int randomTal = rand.nextInt(100);
+		 int randomTal = rand.nextInt(1000);
+		 int Gissning = -1;
+	
+	
+		 while (Gissning !=randomTal) {
+			 
+			 System.out.println("gissa på ett tal mellan 0-1000");
+			 
+			 Gissning = YEET.nextInt();
+			 
+			 if (Gissning>randomTal) {
+				 
+				 System.out.println("För högt! försök igen");
+			 }
+			 
+			 else if (Gissning<randomTal) {
+				 
+				 System.out.println("För lågt försök igen");
+			 }
+			 
+			 else if (Gissning==randomTal) {
+				 
+				 System.out.println("EZ GAME EZ LIFE " +  randomTal );
+				 
+				 
+			 }
+		 }
 		 
-	
-	
+		 
+		 
 	}
 	
 	
