8757306606ccd86aa7b415c1b7c0773a6d08f053
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2019-03-25 22:00:05 +0100

    Har gjord det mesta av spelet
    som att byta ut bokstäver med _ och tillbaka till bokstäver med mera

--- src/lektion_projekt/Hangman.java
+++ src/lektion_projekt/Hangman.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Hangman {
 
-public static void main() {
+public static void main(String[] args) {
 	
 	Scanner jaok = new Scanner(System.in);
 	
@@ -23,11 +23,87 @@ public static void main() {
 			
 			
 			int antalGissningar = ord.length;
+			
+			//gör om bokstäver till '_'
+			char[] usrGuess = new char [antalGissningar];
+			
+			for (int i = 0; i < usrGuess.length; i++ ) {
+				
+				usrGuess[i] = '_';
+			}
 				
 			
+			boolean gissatRätt = false;
+			int försök = 0;
 			
+			while (!gissatRätt && försök !=antalGissningar ) {
+				
+				System.out.print("Gissningar: ");
+				printArray(usrGuess);
+				System.out.println("skriv en bokstav:");
+				char input = Scanner.nextLine().charAt(0);
+				försök++;
+				
+				
+				
+				if ( input == '_') {
+					
+					ViSpelar = false;
+					gissatRätt = true;
+				} else {
+					
+					for (int i = 0; i < ord.length; i++ ) {
+						
+						if(ord[i] == input) {
+							
+							usrGuess[i] = input;
+						}
+					}
+				
+					if (ärOrdetGissat(usrGuess)) {
+						
+						gissatRätt = true;
+						System.out.println("EZ");
+					}
+				
+				}
+				
+				
+			}
+			
+			if(!gissatRätt) System.out.println("TRAAAASHH");
 		}
 		
 		System.out.println( "Fan vad dålig :)" );
 	}	
+
+		public static void printArray (char[] array) {
+			
+			
+				for (int i = 0; i < array.length; i++ ) {
+				
+				System.out.println(array[i] + " ");
+			}
+			
+		}
+	
+		
+		public static boolean ärOrdetGissat(char[] array) {
+			
+			
+				for (int i = 0; i < array.length; i++ ) {
+				
+				if (array[i] == '_') {
+					
+					return false;
+				
+				}
+					return true;
+				
+				}
+				
+				
+		}
 }
+
+
